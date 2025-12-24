c4d5ec17b9c1678af70244f04bb6450bdaf09d36
ZachMGH <zachariemoughanim@gmail.com>
2025-12-24 15:28:33 +0100

    error 400 and favicon support

--- http.py
+++ http.py
@@ -16,34 +16,45 @@ def str_of_path(path : str) -> str :
       res += line
   return res
 
+def bytes_of_path(path : str) -> bytes :
+  """ Returns as bytes the content of the file given by [path] """
+  res : bytes
+  with open(path, mode = "rb") as f :
+    res = f.read()
+  return res
+
 def get_http_time() -> str :
   """ Return GMT time, in the format for HTTP responses """
   cur_time = time.asctime(time.gmtime()).split(' ')
   weekday, month, day, hour, year = cur_time[0], cur_time[1], cur_time[2], cur_time[3], cur_time[4]
   return f"{weekday}, {day} {month} {year} {hour} GMT"
 
-def parse_http(text : str) -> str :
-  """ Parse HTTP query, returns the path to the requested file, or raise an error if malformed or unanswerable query. """
+def parse_http(text : str) -> tuple[int, str] :
+  """ Parse HTTP query, returns the path to the requested file, or raise an error if malformed or unanswerable query.
+      Returns (0, value) if succeeded, (1, error_str) otherwise """
   query = [x.strip('\r').split(' ') for x in text.split('\n')]
   if debug :
     print(query)
   head = query[0]
   for line in query[1:] :
     if line[0] == "Accept:" :
+      print(line[1])
       if "text/html" in line[1].split(',') :
         break
+      if "image/png" in line[1].split(',') :
+        break
   else :
-    raise ValueError("Client does not accept html files.")
+    return (1, "Client does not accept html files or favicon.")
   if len(head) != 3 or head[0] != "GET" or head[2] != "HTTP/1.1":
-    raise (ValueError("First line of HTTP request of wrong format."))
+    raise (1, "First line of HTTP request of wrong format.")
   else :
     path = head[1]
     if not(check_path_subfolder(path)) :
-      raise (ValueError("Trying to access parent folder."))
-    return "."+path
+      return (1, "Trying to access parent folder.")
+    return (0, "."+path)
 
-def http_response(text : str) -> str :
-  ## Header of the response, if everything goes well
+def http_response(text : str) -> bytes :
+  ## Header of the response, if everything goes well TODO return bytes instead of string
   # HTTP/1.1 200 OK
   # Date : {get_http_time()}
   # Content-Type:  text/html;charset=utf-8
@@ -51,16 +62,27 @@ def http_response(text : str) -> str :
   # Content-Language: fr-FR
   # 
   try :
-    path = parse_http(text)
-    body = str_of_path(path)
-    header = f"HTTP/1.1 200 OK\nDate : {get_http_time()}\nContent-Type:  text/html;charset=utf-8\nServer: Tresfreroserver\nContent-Language: fr-FR\nContent-Length: {len(body.encode('utf-8'))}"
+    errcode, parsing_res = parse_http(text)
+    if errcode == 0 :
+      path = parsing_res
+      body = bytes_of_path(path)
+      header = f"HTTP/1.1 200 OK\nDate : {get_http_time()}\nContent-Type:  text/html;charset=utf-8\nServer: Tresfreroserver\nContent-Language: fr-FR\nContent-Length: {len(body)}"
+      return (header + "\n\n").encode() + body + ("\n\n\n\n").encode()
+    else :
+      print(text)
+      print("Caused following error :")
+      print(parsing_res)
+      body = str_of_path("./error400.html")
+      header = f"HTTP/1.1 400\nDate : {get_http_time()}\nContent-Type:  text/html;charset=utf-8\nServer: Tresfreroserver\nContent-Language: fr-FR\nContent-Length: {len(body.encode('utf-8'))}"
+      return (header + "\n\n" + body + "\n\n\n\n").encode()
   except IsADirectoryError :
     body = str_of_path("./error404.html")
     header = f"HTTP/1.1 404\nDate : {get_http_time()}\nContent-Type:  text/html;charset=utf-8\nServer: Tresfreroserver\nContent-Language: fr-FR\nContent-Length: {len(body.encode('utf-8'))}"
+    return (header + "\n\n" + body + "\n\n\n\n").encode()
   except FileNotFoundError :
     body = str_of_path("./error404.html")
     header = f"HTTP/1.1 404\nDate : {get_http_time()}\nContent-Type:  text/html;charset=utf-8\nServer: Tresfreroserver\nContent-Language: fr-FR\nContent-Length: {len(body.encode('utf-8'))}"
-  return header + "\n\n" + body + "\n\n\n\n"
+    return (header + "\n\n" + body + "\n\n\n\n").encode()
 
 # TODO we must treat the request for favicon :
 # GET /favicon.ico undefined
--- server.py
+++ server.py
@@ -5,22 +5,19 @@ file : str = "<!DOCTYPE html><html><head><title>Test</title><meta charset='utf-8
 bidule = f"HTTP/1.1 200 OK\nServer: nginx/1.10.3 (ubuntu)\nDate: Tue, 23 December 2025 14:18:18 GMT\nContent-Type: text/html\nContent-Lenght: {len(file)}\nLast-Modified : Tue, 23 December 2025 14:18:18 GMT\n\n{file}\n\n"
 
 serveur = socket()
-serveur.bind(('0.0.0.0', 9997))
+serveur.bind(('0.0.0.0', 9998))
 serveur.listen()
 
 try :
   while True :
     (sclient, adclient) = serveur.accept()
     donnee = sclient.recv(1000).decode()
-    print("Just received:\n\n\n")
-    print(donnee)
-    print("\n\n\n")
     rep = ""
-    sclient.send(http_response(donnee).encode('utf-8'))
+    sclient.send(http_response(donnee))
     try :
       while True :
         donnee = sclient.recv(1000).decode()
-        sclient.send(http_response(donnee).encode('utf-8'))
+        sclient.send(http_response(donnee))
         print(donnee)
     except KeyboardInterrupt :
       pass
